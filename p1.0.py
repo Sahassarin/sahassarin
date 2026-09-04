edbe43d73ead6f0c6a9cca08a81dcffe5b39145a
Sahassarin <sahatsarin.l@ku.th>
2026-09-04 15:10:25 +0700

    week 10 (work 4/9/69)

---
+++ p1.0.py
@@ -0,0 +1,7 @@
+test_str = "Hello123"
+print(f"\nValidation methods for '{test_str}':")
+print(f"isalnum(): {test_str.isalnum()}")
+print(f"isalpha(): {test_str.isalpha()}")
+print(f"isdigit(): {test_str.isdigit()}")
+print(f"isupper(): {test_str.isupper()}")
+print(f"islower(): {test_str.islower()}")
